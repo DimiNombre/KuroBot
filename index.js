ae091fa84a5ac968bc68e9fab8192293cac9b8b9
Damián Fraire <damianfluengo@gmail.com>
2021-01-11 14:35:05 +0100

    Added processes

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ client.on("message", async(message) => {
                 try{
                     let connection = await vc.join();
                     queueConstructor.connection = connection;
-                    message.guild.me.voice.setSelfMute(true);
+                    message.guild.me.voice.setSelfDeaf(true);
                     play(message.guild, queueConstructor.songs[0]);
                 }catch (err){
                     console.error(err);
